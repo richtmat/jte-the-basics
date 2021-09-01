49c44d3f1719bded3cdd2f038f76a94157f33f40
Matthias Richter <matthiasr@gmail.com>
2021-09-01 15:01:56 +0200

    Update build.groovy

--- libraries/maven/steps/build.groovy
+++ libraries/maven/steps/build.groovy
@@ -1,7 +1,6 @@
-void call(){
-    
+void call(){    
     stage("Maven: Build"){
         println "build from the maven libraryyyyy"
-      //  withDockerContainer("maven:3.5.0-jdk-8-alpine") { sh "mvn -v"}        
+        //  withDockerContainer("maven:3.5.0-jdk-8-alpine") { sh "mvn -v"}        
     }
 }
